a6dace2b7ad0f9d60c4907d18291684697a58858
xenxei46 <wisdomjohnikoi@gmail.com>
2024-01-30 22:08:21 +0100

    buyer ui

---
+++ app/dashboard/Pages/makePayment.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MakePayment = () => {
+  return (
+    <View>
+      <Text>makePayment</Text>
+    </View>
+  )
+}
+
+export default MakePayment
